125cc69d3713fc7bb4c6bbd51756f504a93f231e
vidakpop <daviskpop@gmail.com>
2025-04-13 12:24:19 +0300

    Add jazzmin to installed apps for enhanced admin interface

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
