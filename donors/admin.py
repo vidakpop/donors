c809a3559f1e1dad705d72d67cecd310038b5562
vidakpop <daviskpop@gmail.com>
2025-04-13 12:12:42 +0300

    Register models in the admin interface for DonorProfile, Project, ProjectUpdate, Donation, and Notification

--- donors/admin.py
+++ donors/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from .models import DonorProfile, Project, ProjectUpdate, Donation, Notification
 
 
+admin.site.register(DonorProfile)
+admin.site.register(Project)
+admin.site.register(ProjectUpdate)
+admin.site.register(Donation)
+admin.site.register(Notification)
