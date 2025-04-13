a0dd2f41e04a8170f88fc5c3f10c543e85ee95c6
vidakpop <daviskpop@gmail.com>
2025-04-13 11:34:57 +0300

    Add DonorProfile model with user relationship, profile picture, and bio fields

--- donors/models.py
+++ donors/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class DonorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 # Create your models here.
