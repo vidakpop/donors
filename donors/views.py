94d23d92d771e8407b88ef6bfb9ab66653d74564
vidakpop <daviskpop@gmail.com>
2025-04-15 11:00:56 +0300

    Refactor views.py to import necessary modules for view handling

--- donors/views.py
+++ donors/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets,generics,permissions,status
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
