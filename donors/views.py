90807b4aae44730b16a4e22953e980d4f7b27592
vidakpop <daviskpop@gmail.com>
2025-04-15 11:10:34 +0300

    Add ProjectListView and ProjectDetailView for project listing and details

--- donors/views.py
+++ donors/views.py
@@ -4,3 +4,34 @@ from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
+# Donor >> Project listing and details
+class ProjectListView(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ProjectDetailView(generics.RetrieveAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+#NGO >> Create and Edit Project
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+class ProjectUpdateViewSet(viewsets.ModelViewSet):
+    queryset = ProjectUpdate.objects.all()
+    serializer_class = ProjectUpdateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
