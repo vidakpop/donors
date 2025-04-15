d294c3b52e1db89ecbf8f2a632842954ba3e9232
vidakpop <daviskpop@gmail.com>
2025-04-15 13:39:48 +0300

    Add URL routing for projects and project updates in the donors app

--- donors/urls.py
+++ donors/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . views import *
+
+router = DefaultRouter()
+router.register('projects', ProjectViewSet)
+router.register('project-updates', ProjectUpdateViewSet)
+
