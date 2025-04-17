49af8c769105c53cf11d9e22f4279f4060527fe9
vidakpop <daviskpop@gmail.com>
2025-04-17 20:48:22 +0300

    Implement SignUpView with POST method for user registration

--- authentication/views.py
+++ authentication/views.py
@@ -5,3 +5,13 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+
+class SignUpView(APIView):
+    permission_classes= [AllowAny]
+
+    def post(self, request):
+        username=request.data.get('username')
+        email=request.data.get('email')
+        password=request.data.get('password')
+        
+        
