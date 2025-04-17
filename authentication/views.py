6fc9238b76aa750727e8e37f11139517fa1db51e
vidakpop <daviskpop@gmail.com>
2025-04-17 20:45:14 +0300

    Add initial imports and setup for authentication views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
