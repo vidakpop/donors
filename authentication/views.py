ff75796faed6f5a52c8ea0d3908f49ef087728e7
vidakpop <daviskpop@gmail.com>
2025-04-17 20:51:47 +0300

    Refactor SignUpView POST method to create user and return tokens

--- authentication/views.py
+++ authentication/views.py
@@ -16,3 +16,12 @@ class SignUpView(APIView):
         
         if User.objects.filter(username=username).exists():
             return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user=User.objects.create_user(username=username, email=email, password=password)
+        refresh=RefreshToken.for_user(user)
+
+        return Response({
+            "username": user.username,
+            "access_token": str(refresh.access_token),
+            "refresh_token": str(refresh),
+        })
