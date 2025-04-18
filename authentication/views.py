fa88409880b3c897207ceccbf8ca82795b6441cd
vidakpop <daviskpop@gmail.com>
2025-04-18 16:57:41 +0300

    Implement initial LoginView with POST method for user authentication

--- authentication/views.py
+++ authentication/views.py
@@ -25,3 +25,18 @@ class SignUpView(APIView):
             "access_token": str(refresh.access_token),
             "refresh_token": str(refresh),
         })
+    
+class LoginView(APIView):
+    permission_classes= [AllowAny]
+
+    def post(self, request):
+        username=request.data.get('username')
+        password=request.data.get('password')
+
+        try:
+            user=User.objects.get(username=username)
+
+            if not user.check_password(password):
+                return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            
